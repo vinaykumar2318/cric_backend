f97edeea912f31db193357894f540a919765aa77
Vinay Kumar <138013792+vinaykumar2318@users.noreply.github.com>
2025-08-13 22:50:56 +0530

    Update util.py

--- util.py
+++ util.py
@@ -95,16 +95,17 @@ if __name__ == '__main__':
     print("running1")
     # print(classify_image(get_b64_test_image_for_virat(), None))
 
-    print(classify_image(None, "./test_images/virat_kohli2.png"))
-    print(classify_image(None, "./test_images/rohit_sharma2.png"))
-    print(classify_image(None, "./test_images/ms_dhoni14.png"))
-    print(classify_image(None, "./test_images/virat_kohli6.png"))
-    print(classify_image(None, "./test_images/kl_rahul6.png"))
-    print(classify_image(None, "./test_images/shubhman_gill3.png"))
+    # print(classify_image(None, "./test_images/virat_kohli2.png"))
+    # print(classify_image(None, "./test_images/rohit_sharma2.png"))
+    # print(classify_image(None, "./test_images/ms_dhoni14.png"))
+    # print(classify_image(None, "./test_images/virat_kohli6.png"))
+    # print(classify_image(None, "./test_images/kl_rahul6.png"))
+    # print(classify_image(None, "./test_images/shubhman_gill3.png"))
     # print(classify_image(None, "./test_images/federer2.jpg"))
     # print(classify_image(None, "./test_images/virat1.jpg"))
     # print(classify_image(None, "./test_images/virat2.jpg"))
     # print(classify_image(None, "./test_images/virat3.jpg")) # Inconsistent result could be due to https://github.com/scikit-learn/scikit-learn/issues/13211
     # print(classify_image(None, "./test_images/serena1.jpg"))
     # print(classify_image(None, "./test_images/serena2.jpg"))
+
     # print(classify_image(None, "./test_images/sharapova1.jpg"))
